727ff8c5ad85cee2ec3474fedfedbc0b40dd89d7
Sushant <cashu853@gmail.com>
2024-07-03 15:02:25 +0530

    jwt token

--- login-sign up/frontend/src/App.jsx
+++ login-sign up/frontend/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
   <>
    <div className="flex items-center my-auto   gap-4 justify-center">
-
       <button className=' bg-purple-500 px-3 py-2 rounded-full hover:bg-purple-400  transition-all  duration-700'>Sign Up</button>
       <button className=' bg-purple-500 px-3 py-2 rounded-full hover:bg-purple-400  transition-all  duration-700'>Login</button>
    </div>
